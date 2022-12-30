4e8352783e979cebf8417bd61760a79459330f58
Rufat Abdurahmanov <abdurahmanov.r.n@gmail.com>
2022-12-30 12:56:07 +0100

    All starts with one dot
    Draw a dot and save it to the file

---
+++ 3drenderer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+int width = 100;
+int height = 100;
+
+Bitmap image = new Bitmap(width, height);
+image.SetPixel(52, 41, Color.Red);
+image.RotateFlip(RotateFlipType.RotateNoneFlipY); // I want to have the origin at the left bottom corner of the image
+
+
+Stream stream = File.Create("output.bmp");
+image.Save(stream, ImageFormat.Bmp);
+stream.Close();
+
