707648d0422b2da24ecc223f3c1caea803945633
Rufat Abdurahmanov <abdurahmanov.r.n@gmail.com>
2023-01-02 11:56:37 +0100

    Draw a line
    Draw a line between two points with arbitrary choosen constant increment factor.

--- 3drenderer/Program.cs
+++ 3drenderer/Program.cs
@@ -5,11 +5,26 @@ int width = 100;
 int height = 100;
 
 Bitmap image = new Bitmap(width, height);
-image.SetPixel(52, 41, Color.Red);
+
+DrawLine(13, 20, 80, 40, image, Color.White);
+
 image.RotateFlip(RotateFlipType.RotateNoneFlipY); // I want to have the origin at the left bottom corner of the image
 
 
 Stream stream = File.Create("output.bmp");
 image.Save(stream, ImageFormat.Bmp);
 stream.Close();
+
+
+void DrawLine(int x0, int y0, int x1, int y1, Bitmap image, Color color)
+{
+    const double dt = 0.01;
+
+    for (double t = 0; t < 1.0; t += dt)
+    {
+        int x = (int)(x0 + (x1 - x0) * t);
+        int y = (int)(y0 + (y1 - y0) * t);
+        image.SetPixel(x, y, color);
+    }
+}
 
